2935672fc8f0ac9624baba85ee07ae18225c7746
Jophkins <sergebrew@gmail.com>
2022-11-24 18:00:37 +0300

    layout 'homepage' header, episodes, host, form

---
+++ src/components/Host/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import styles from './Host.module.scss';
+
+const Host = () => {
+  return (
+    <section className={styles.sectionHost}>
+      <div className="section-inner">
+        <div className={`${styles.sectionHostWrapper} container`}>
+          <div className="row">
+            <div className="col-12 col-lg-6 d-flex flex-column justify-content-center">
+              <div className={styles.sectionHostWrapperTitle}>
+                Jacob Paulaner
+              </div>
+              <div className={styles.sectionHostWrapperDescription}>
+                Jacob has a background in audio engineering, and has been podcasting since the early days.
+                <br/><br/>
+                  He’s here to help you level up your game by sharing everything he’s learned along the way.
+              </div>
+              <div className={styles.sectionHostWrapperBtnBlock}>
+                <img className="img-fluid" src={require('../../assets/img/host-btn.png')} alt=""/>
+                  <span>Meet your host</span>
+              </div>
+            </div>
+            <div className={`col-12 col-lg-6 ${styles.sectionHostWrapperImage} p-lg-5`}>
+              <img className="img-fluid" src={require('../../assets/img/host-img.jpg')} alt=""/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Host;
